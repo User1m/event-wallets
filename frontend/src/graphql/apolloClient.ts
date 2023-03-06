c92d43c538f3251052661be820d50f71a8327670
Claudius Mbemba <mbembac@gmail.com>
2023-03-06 15:39:47 -0800

    fixes

--- frontend/src/graphql/apolloClient.ts
+++ frontend/src/graphql/apolloClient.ts
@@ -1,14 +1,12 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
 const uri = window.location.href.includes('https://')
-  ? process.env.GRAPHQL_URL
+  ? 'https://event-wallets-api.herokuapp.com/graphql' // process.env.GRAPHQL_URL
   : process.env.DEV_GRAPHQL_URL
 
-console.log('uri', uri)
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({ uri })
-  // link: new HttpLink({ uri: 'https://event-wallets-api.herokuapp.com/graphql' })
 })
 
 export default client
